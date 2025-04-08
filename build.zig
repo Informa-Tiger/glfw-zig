ae94030709d22a6897f492031cbd4607a2a496f7
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2025-04-08 13:18:21 +0100

    build.zig

--- build.zig
+++ build.zig
@@ -1,63 +1,26 @@
-const b..const std = @import("std");
+const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
+    const upstream = b.dependency("glfw", .{});
+
+    const mod = b.createModule(.{
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe_mod.addImport("glfw_zig_lib", lib_mod);
-
-    const lib = b.addLibrary(.{
+    const glfw = b.addLibrary(.{
         .linkage = .static,
-        .name = "glfw_zig",
-        .root_module = lib_mod,
+        .name = "glfw",
+        .root_module = mod,
     });
 
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "glfw_zig",
-        .root_module = exe_mod,
+    mod.addCSourceFiles(.{
+        .root = upstream.path("src"),
     });
 
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    b.installArtifact(glfw);
 }
